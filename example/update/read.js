69cdae58768d7fcdd35ab8725c0f73e339c875e6
Rainame <wzxjjz@gmail.com>
2014-08-21 21:11:24 +0800

    添加或替换类方法注释

--- example/update/read.js
+++ example/update/read.js
@@ -9,8 +9,8 @@ var debug = require('debug')('blog:update:read');
  * @param {String} url
  * @param {Function} callback
  */
-function request (url, callback) {
-  originRequest(url, callback);
+function request(url, callback) {
+    originRequest(url, callback);
 }
 
 
@@ -20,34 +20,35 @@ function request (url, callback) {
  * @param {String} url
  * @param {Function} callback
  */
-exports.classList = function (url, callback) {
-  debug('读取文章分类列表：%s', url);
+exports.classList = function(url, callback) {
+    debug('读取文章分类列表：%s', url);
 
-  request(url, function (err, res) {
-    if (err) return callback(err);
+    request(url, function(err, res) {
+        if (err)
+            return callback(err);
 
-    // 根据网页内容创建DOM操作对象
-    var $ = cheerio.load(res.body.toString());
+        // 根据网页内容创建DOM操作对象
+        var $ = cheerio.load(res.body.toString());
 
-    // 读取博文类别列表
-    var classList = [];
-    $('.classList li a').each(function () {
-      var $me = $(this);
-      var item = {
-        name: $me.text().trim(),
-        url:  $me.attr('href')
-      };
-      // 从URL中取出分类的ID
-      var s = item.url.match(/articlelist_\d+_(\d+)_\d\.html/);
-      if (Array.isArray(s)) {
-        item.id = s[1];
-        classList.push(item);
-      }
+        // 读取博文类别列表
+        var classList = [];
+        $('.classList li a').each(function() {
+            var $me = $(this);
+            var item = {
+                name: $me.text().trim(),
+                url: $me.attr('href')
+            };
+            // 从URL中取出分类的ID
+            var s = item.url.match(/articlelist_\d+_(\d+)_\d\.html/);
+            if (Array.isArray(s)) {
+                item.id = s[1];
+                classList.push(item);
+            }
+        });
+
+        // 返回结果
+        callback(null, classList);
     });
-
-    // 返回结果
-    callback(null, classList);
-  });
 };
 
 /**
@@ -56,49 +57,51 @@ exports.classList = function (url, callback) {
  * @param {String} url
  * @param {Function} callback
  */
-exports.articleList = function (url, callback) {
-  debug('读取博文列表：%s', url);
+exports.articleList = function(url, callback) {
+    debug('读取博文列表：%s', url);
 
-  request(url, function (err, res) {
-    if (err) return callback(err);
+    request(url, function(err, res) {
+        if (err)
+            return callback(err);
 
-    // 根据网页内容创建DOM操作对象
-    var $ = cheerio.load(res.body.toString());
+        // 根据网页内容创建DOM操作对象
+        var $ = cheerio.load(res.body.toString());
 
-    // 读取博文列表
-    var articleList = [];
-    $('.articleList .articleCell').each(function () {
-      var $me = $(this);
-      var $title = $me.find('.atc_title a');
-      var $time = $me.find('.atc_tm');
-      var item = {
-        title: $title.text().trim(),
-        url:   $title.attr('href'),
-        time:  $time.text().trim()
-      };
-      // 从URL中取出文章的ID
-      var s = item.url.match(/blog_([a-zA-Z0-9]+)\.html/);
-      if (Array.isArray(s)) {
-        item.id = s[1];
-        articleList.push(item);
-      }
+        // 读取博文列表
+        var articleList = [];
+        $('.articleList .articleCell').each(function() {
+            var $me = $(this);
+            var $title = $me.find('.atc_title a');
+            var $time = $me.find('.atc_tm');
+            var item = {
+                title: $title.text().trim(),
+                url: $title.attr('href'),
+                time: $time.text().trim()
+            };
+            // 从URL中取出文章的ID
+            var s = item.url.match(/blog_([a-zA-Z0-9]+)\.html/);
+            if (Array.isArray(s)) {
+                item.id = s[1];
+                articleList.push(item);
+            }
+        });
+
+        // 检查是否有下一页
+        var nextUrl = $('.SG_pgnext a').attr('href');
+        if (nextUrl) {
+            // 读取下一页
+            exports.articleList(nextUrl, function(err, articleList2) {
+                if (err)
+                    return callback(err);
+
+                // 合并结果
+                callback(null, articleList.concat(articleList2));
+            });
+        } else {
+            // 返回结果
+            callback(null, articleList);
+        }
     });
-
-    // 检查是否有下一页
-    var nextUrl = $('.SG_pgnext a').attr('href');
-    if (nextUrl) {
-      // 读取下一页
-      exports.articleList(nextUrl, function (err, articleList2) {
-        if (err) return callback(err);
-
-        // 合并结果
-        callback(null, articleList.concat(articleList2));
-      });
-    } else {
-      // 返回结果
-      callback(null, articleList);
-    }
-  });
 };
 
 /**
@@ -107,28 +110,29 @@ exports.articleList = function (url, callback) {
  * @param {String} url
  * @param {Function} callback
  */
-exports.articleDetail = function (url, callback) {
-  debug('读取博文内容：%s', url);
+exports.articleDetail = function(url, callback) {
+    debug('读取博文内容：%s', url);
 
-  request(url, function (err, res) {
-    if (err) return callback(err);
+    request(url, function(err, res) {
+        if (err)
+            return callback(err);
 
-    // 根据网页内容创建DOM操作对象
-    var $ = cheerio.load(res.body.toString());
+        // 根据网页内容创建DOM操作对象
+        var $ = cheerio.load(res.body.toString());
 
-    // 获取文章标签
-    var tags = [];
-    $('.blog_tag h3 a').each(function () {
-      var tag = $(this).text().trim();
-      if (tag) {
-        tags.push(tag);
-      }
+        // 获取文章标签
+        var tags = [];
+        $('.blog_tag h3 a').each(function() {
+            var tag = $(this).text().trim();
+            if (tag) {
+                tags.push(tag);
+            }
+        });
+
+        // 获取文章内容
+        var content = $('.articalContent').html().trim();
+
+        // 返回结果
+        callback(null, {tags: tags, content: content});
     });
-
-    // 获取文章内容
-    var content = $('.articalContent').html().trim();
-
-    // 返回结果
-    callback(null, {tags: tags, content: content});
-  });
 };
--- example/web/read.js
+++ example/web/read.js
@@ -8,10 +8,10 @@ var debug = require('debug')('blog:web:read');
  *
  * @param {Function} callback
  */
-exports.classList = function (callback) {
-  debug('获取文章分类列表');
+exports.classList = function(callback) {
+    debug('获取文章分类列表');
 
-  db.query('SELECT * FROM `class_list` ORDER BY `id` ASC', callback);
+    db.query('SELECT * FROM `class_list` ORDER BY `id` ASC', callback);
 };
 
 /**
@@ -20,14 +20,15 @@ exports.classList = function (callback) {
  * @param {Number} id
  * @param {Function} callback
  */
-exports.isClassExists = function (id, callback) {
-  debug('检查分类是否存在：%s', id);
+exports.isClassExists = function(id, callback) {
+    debug('检查分类是否存在：%s', id);
 
-  db.query('SELECT * FROM `class_list` WHERE `id`=? LIMIT 1', [id], function (err, ret) {
-    if (err) return next(err);
+    db.query('SELECT * FROM `class_list` WHERE `id`=? LIMIT 1', [id], function(err, ret) {
+        if (err)
+            return next(err);
 
-    callback(null, Array.isArray(ret) && ret.length > 0);
-  });
+        callback(null, Array.isArray(ret) && ret.length > 0);
+    });
 };
 
 /**
@@ -36,15 +37,17 @@ exports.isClassExists = function (id, callback) {
  * @param {Number} id
  * @param {Function} callback
  */
-exports.class = function (id, callback) {
-  debug('获取指定分类的信息：%s', id);
+exports.class = function(id, callback) {
+    debug('获取指定分类的信息：%s', id);
 
-  db.query('SELECT * FROM `class_list` WHERE `id`=? LIMIT 1', function (err, list) {
-    if (err) return callback(err);
-    if (!(list.length > 0)) return callback(new Error('该分类不存在'));
+    db.query('SELECT * FROM `class_list` WHERE `id`=? LIMIT 1', function(err, list) {
+        if (err)
+            return callback(err);
+        if (!(list.length > 0))
+            return callback(new Error('该分类不存在'));
 
-    callback(null, list[0]);
-  });
+        callback(null, list[0]);
+    });
 };
 
 /**
@@ -53,18 +56,20 @@ exports.class = function (id, callback) {
  * @param {String} id
  * @param {Function} callback
  */
-exports.article = function (id, callback) {
-  debug('获取指定文章的详细信息：%s', id);
+exports.article = function(id, callback) {
+    debug('获取指定文章的详细信息：%s', id);
 
-  var sql = 'SELECT * FROM `article_list` AS `A`' +
+    var sql = 'SELECT * FROM `article_list` AS `A`' +
             ' LEFT JOIN `article_detail` AS `B` ON `A`.`id`=`B`.`id`' +
             ' WHERE `A`.`id`=? LIMIT 1';
-  db.query(sql, [id], function (err, list) {
-    if (err) return callback(err);
-    if (!(list.length > 0)) return callback(new Error('该文章不存在'));
+    db.query(sql, [id], function(err, list) {
+        if (err)
+            return callback(err);
+        if (!(list.length > 0))
+            return callback(new Error('该文章不存在'));
 
-    callback(null, list[0]);
-  });
+        callback(null, list[0]);
+    });
 };
 
 /**
@@ -75,14 +80,14 @@ exports.article = function (id, callback) {
  * @param {Number} limit
  * @param {Function} callback
  */
-exports.articleListByClassId = function (classId, offset, limit, callback) {
-  debug('获取指定分类下的文章列表：%s, %s, %s', classId, offset, limit);
+exports.articleListByClassId = function(classId, offset, limit, callback) {
+    debug('获取指定分类下的文章列表：%s, %s, %s', classId, offset, limit);
 
-  var sql = 'SELECT * FROM `article_list` AS `A`' +
+    var sql = 'SELECT * FROM `article_list` AS `A`' +
             ' LEFT JOIN `article_detail` AS `B` ON `A`.`id`=`B`.`id`' +
             ' WHERE `A`.`class_id`=?' +
             ' ORDER BY `created_time` DESC LIMIT ?,?';
-  db.query(sql, [classId, offset, limit], callback);
+    db.query(sql, [classId, offset, limit], callback);
 };
 
 /**
@@ -93,13 +98,13 @@ exports.articleListByClassId = function (classId, offset, limit, callback) {
  * @param {Number} limit
  * @param {Function} callback
  */
-exports.articleListByTag = function (tag, offset, limit, callback) {
-  debug('获取指定标签下的文章列表：%s, %s, %s', tag, offset, limit);
+exports.articleListByTag = function(tag, offset, limit, callback) {
+    debug('获取指定标签下的文章列表：%s, %s, %s', tag, offset, limit);
 
-  var sql = 'SELECT * FROM `article_list` WHERE `id` IN (' +
+    var sql = 'SELECT * FROM `article_list` WHERE `id` IN (' +
             ' SELECT `id` FROM `article_tag` WHERE `tag`=?)' +
             ' ORDER BY `created_time` DESC LIMIT ?,?';
-  db.query(sql, [tag, offset, limit], callback);
+    db.query(sql, [tag, offset, limit], callback);
 };
 
 /**
@@ -108,12 +113,13 @@ exports.articleListByTag = function (tag, offset, limit, callback) {
  * @param {String} tag
  * @param {Function} callback
  */
-exports.articleCountByTag = function (tag, callback) {
-  debug('获取指定标签下的文章数量：%s', tag);
+exports.articleCountByTag = function(tag, callback) {
+    debug('获取指定标签下的文章数量：%s', tag);
 
-  db.query('SELECT COUNT(*) AS `c` FROM `article_tag` WHERE `tag`=?', [tag], function (err, ret) {
-    if (err) return callback(err);
+    db.query('SELECT COUNT(*) AS `c` FROM `article_tag` WHERE `tag`=?', [tag], function(err, ret) {
+        if (err)
+            return callback(err);
 
-    callback(null, ret[0].c);
-  });
+        callback(null, ret[0].c);
+    });
 };
